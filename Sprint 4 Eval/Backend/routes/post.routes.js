a7621fc40c0de170fde6f36df820877211f36d76
PoojaRathod5 <poojachavan8669@gmail.com>
2023-03-27 15:13:27 +0530

    added update logic

--- Sprint 4 Eval/Backend/routes/post.routes.js
+++ Sprint 4 Eval/Backend/routes/post.routes.js
@@ -3,15 +3,45 @@ const postRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const { PostModel } = require("../model/post.model");
 
-postRouter.get("/", async(req,res)=>{
+// get all posts
+postRouter.get("/", async (req, res) => {
     const token = req.headers.authorization.split(" ")[1];
-    const decoded = jwt.verify(token,"masai");
-    try{
-        if(decoded){
-            const posts= await PostModel.find({"userID":decoded.userID});
+    const decoded = jwt.verify(token, "masai");
+    try {
+        if (decoded) {
+            const posts = await PostModel.find({ "userID": decoded.userID });
             res.status(200).send(posts);
         }
-    }catch(err){
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+})
+
+//add a new post
+postRouter.post("/add", async (req, res) => {
+    try {
+        const post = new PostModel(req.body);
+        await post.save();
+        res.status(200).send({ "msg": "A new post has been added" })
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+})
+
+//update post
+postRouter.post("/", async (req, res) => {
+    try {
+            
+    } catch (err) {
+        res.status(400).send(err.message);
+    }
+})
+
+//delete a post
+postRouter.post("/", async (req, res) => {
+    try {
+
+    } catch (err) {
         res.status(400).send(err.message);
     }
 })

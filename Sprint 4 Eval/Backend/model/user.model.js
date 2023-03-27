54759a33c937e1c4b4220552ce2134c8052b04db
PoojaRathod5 <poojachavan8669@gmail.com>
2023-03-27 14:54:24 +0530

    added registration logic

--- Sprint 4 Eval/Backend/model/user.model.js
+++ Sprint 4 Eval/Backend/model/user.model.js
@@ -1,18 +1,18 @@
 //creatin model for declaring schema of Users
 const mongoose = require("mongoose");
 
-const userSchema= mongoose.Schema({
+const userSchema = mongoose.Schema({
     name: String,
     email: String,
     gender: String,
     password: String,
     age: Number,
     city: String,
-    is_married: boolean
+    is_married: Boolean
 })
 
 //delcaring model
-const UserModel= mongoose.model("user",userSchema);
+const UserModel = mongoose.model("user", userSchema);
 
 module.exports = {
     UserModel
--- Sprint 4 Eval/Backend/routes/user.routes.js
+++ Sprint 4 Eval/Backend/routes/user.routes.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const userRouter = express.Router();
-
-
+const { UserModel } = require("../model/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+//registration
 userRouter.post("/register", async (req, res) => {
-    const { email, pass, location, age } = req.body;
-    console.log(email, pass, location, age);
+    const { name, email, gender, password, age, city, is_married } = req.body;
+    try {
+        //check if user already exists
+        const userExists = await UserModel.findOne({ email })
+        if (userExists) {
+            return res.status(400).send({ "msg": "User already exist, please login" })
+        } else {
+            bcrypt.hash(password, 5, async (err, hash) => {
+                const user = new UserModel({ name, email, gender, password: hash, age, city, is_married })
+                await user.save();
+                res.status(200).send({ "msg": "Registration Successful" })
+            })
+        }
+    } catch (err) {
+        res.status(400).send({ "msg": err.message });
+    }
+
 })
 
 module.exports = {
